6d937d7c2523607a26a48cca2d91f5ac302aaf33
Josué Caleb a.k.a JCaleb <josshua_do_desert@live.com>
2017-05-11 11:26:54 -0300

    [IN PROGRESS] v0.2 Simple GUI Implementation C
    -More Progress made

--- stellaris1GUI.py
+++ stellaris1GUI.py
@@ -11,28 +11,28 @@ from zipfile    import ZipFile  as ZipFile
 class mainCode:
     pyPath = getcwd()
 
-    def unzipper(self, version):
+    def unzipper(self):
+        version = "1.5.*"
         for l in listdir():
             if l.endswith(".zip") and l != "base_library.zip":
                 nl = l.replace(".zip", "")
                 with ZipFile(l, 'r') as ex:
                     mkdir(nl)
                     ex.extractall(nl)
-                    mainCode.editor(nl, version)
+                    mainCode.editor(self, nl, version)
             else:
                 pass
 
     def editor(self, nl, version):
         chdir(nl)
-        with open("descriptor.mod", "w") as fm:
-            fm.write("name="+"\""+nl+"\"")
-            fm.write("\npath="+"\""+"mod/"+nl+"\"")
-            fm.write("\nsupported_version="+"\""+version+"\"")
+        with open("descriptor.mod", "w") as mod:
+            mod.write("name="+"\""+nl+"\"")
+            mod.write("\npath="+"\""+"mod/"+nl+"\"")
+            mod.write("\nsupported_version="+"\""+version+"\"")
         nnl = nl+".mod"
         move("descriptor.mod", nnl)
         move(nnl, mainCode.pyPath)
         chdir(mainCode.pyPath)
-
 
 class mainWindow:                                                       # Class that will be responsible for GUI
     def __init__(self, toplevel):                                       # Initialization function, inits all other widgets/functions
@@ -49,35 +49,27 @@ class mainWindow:                                                       # Class 
             self.label4 = Label(self.frm, text="then move it outside their own folders to root mod folder.")
             self.label5 = Label(self.frm, text="\nPS: Please use _ (underscore) instead of spacebar on zip filenames, just to be safe!!!")
             self.label6 = Label(self.frm, text="\n\nProceed to installation process? (Y/N)")
-            chc = ""
-            self.entry1 = Entry(self.frm, width="3", textvariable=chc)
-            def clique1(self, chc):
-                if chc == "Y" or chc == "y":
-                    pass
-                elif chc == "N" or chc == "n":
-                    root.destroy()
-            
-            self.entry1.focus_force()
             self.label1.pack()
             self.label2.pack()
             self.label3.pack()
             self.label4.pack()
             self.label5.pack()
             self.label6.pack()
-            self.entry1.pack()
-
-            return chc
 
         def initButtons(self, parent=None):                             # Responsible for initializing all buttons
-            self.button1 = Button(self.frm, text="OK!")
-            self.button1.bind("<Button-1>", self.clique1)
+            def click1(self):
+                mainCode.unzipper(self)
+
+            self.button1 = Button(self.frm, text="Start!")
+            self.button1.bind("<Button-1>", click1(self))
             self.button1.pack()
+            
+        #def initB(self, parent=None):
 
-        #def initB(self, parent=__init__):
-
-        #def initC(self, parent=__init__):
+        #def initC(self, parent=None):
         initLabels(self)
         initButtons(self)
+
 root=Tk()
 mainWindow(root)
 root.title("Stellaris Mod Installer")
